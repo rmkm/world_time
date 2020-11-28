b30554421f4aae3f84bcf225da515c58b14e016c
rmkm <rmkm912@gmail.com>
2020-11-28 23:51:02 +0900

    add WorldTime class

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -7,27 +7,6 @@ class ChooseLocation extends StatefulWidget {
 
 class _ChooseLocationState extends State<ChooseLocation> {
 
-  void getData() async {
-    // simulate network request for a username
-    String username = await Future.delayed(Duration(seconds: 3), () {
-      return 'yoshi';
-    });
-
-    String bio = await Future.delayed(Duration(seconds: 2), () {
-      return 'vegan, musician';
-    });
-
-    print('$username - $bio');
-
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    getData();
-    print('initState ran');
-  }
-
   @override
   Widget build(BuildContext context) {
     print('build function ran');
