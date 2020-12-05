4e44dddced471352fb828cfc6ae99034cc81e94b
rmkm <rmkm912@gmail.com>
2020-12-05 23:52:56 +0900

    add showing date at home screen

--- lib/services/world_time.dart
+++ lib/services/world_time.dart
@@ -1,5 +1,6 @@
 import 'package:http/http.dart';
 import 'dart:convert';
+import 'package:intl/intl.dart';
 
 class WorldTime {
 
@@ -11,22 +12,27 @@ class WorldTime {
   WorldTime({this.location, this.flag, this.url});
 
   Future<void> getTime() async {
-    Response response = await get('http://worldtimeapi.org/api/timezone/$url');
-    Map data = jsonDecode(response.body);
-    //print(data);
+    try {
+      Response response = await get('http://worldtimeapi.org/api/timezone/$url');
+      Map data = jsonDecode(response.body);
+      //print(data);
 
-    // get properties from data
-    String datetime = data['datetime'];
-    String offset = data['utc_offset'].substring(1,3);
-    //print(datetime);
-    //print(offset);
+      // get properties from data
+      String datetime = data['datetime'];
+      String offset = data['utc_offset'].substring(1,3);
+      //print(datetime);
+      //print(offset);
 
-    // create DateTime object
-    DateTime now = DateTime.parse(datetime);
-    now = now.add(Duration(hours: int.parse(offset)));
+      // create DateTime object
+      DateTime now = DateTime.parse(datetime);
+      now = now.add(Duration(hours: int.parse(offset)));
 
-    // set the time property
-    time = now.toString();
-
+      // set the time property
+      time = DateFormat.jm().format(now);
+    }
+    catch (e) {
+      print('caught error: $e');
+      time = 'could not get time data';
+    }
   }
 }
