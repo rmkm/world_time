44625d904898fa224afef8a780792a5b69df8c19
rmkm <rmkm912@gmail.com>
2020-12-19 23:11:05 +0900

    add country images

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -10,7 +10,7 @@ class Loading extends StatefulWidget {
 class _LoadingState extends State<Loading> {
 
   void setupWorldTime() async {
-    WorldTime instance = WorldTime(location: 'Berlin', flag: 'germany.png', url: 'Europe/Berlin');
+    WorldTime instance = WorldTime(location: 'London', flag: 'uk.png', url: 'Europe/London');
     await instance.getTime();
     Navigator.pushReplacementNamed(context, '/home', arguments: {
       'location': instance.location,
